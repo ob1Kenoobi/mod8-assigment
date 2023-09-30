9442880d12d75b4e9bf162216786d618503981c5
Hasan Banna <hansolo132134@gmail.com>
2023-09-30 12:24:18 +0600

    added a new dart file for image path.

---
+++ lib/image_paths.dart
@@ -0,0 +1,14 @@
+class ImagePaths {
+  static const List<String> paths = [
+    'assets/images/photo.jpg',
+    'assets/images/photo1.jpg',
+    'assets/images/photo2.jpg',
+    'assets/images/photo3.jpg',
+    'assets/images/photo4.jpg',
+    'assets/images/photo5.jpg',
+    'assets/images/profile.jpg',
+    'assets/images/profile.jpg',
+    'assets/images/profile.jpg',
+    'assets/images/profile.jpg',
+  ];
+}
